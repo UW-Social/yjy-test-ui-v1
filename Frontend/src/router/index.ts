fc8fd265077b946acf0f2d35667f3bd328216760
DengDuangLang111 <yanjiayuan747@gmail.com>
2025-10-25 20:53:14 -0700

    Fix router base path for GitHub Pages

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -67,8 +67,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  // Cool, better than createWebHistory.
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
